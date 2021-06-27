187bf8a7e34d0a2b0e983ca322262d445534eb37
Man Nguyen <nguyenlyminhman@gmail.com>
2021-06-27 14:46:57 +0700

    signup

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,20 @@
 import { EntityRepository, Repository } from "typeorm";
+import { AuthCredentialsDTO } from "./dto/auth.credentials.dto";
 import { User } from "./user.entity";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User>{
+
+    async signUp(authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
+        const { username, password } = authCredentialsDTO;
+        const user = new User();
+        user.username = username;
+        user.password = password;
+        await user.save();
+    }
+
+    async signIn() {
+
+    }
 
 }
